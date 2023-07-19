3c41d685dff933f8440be7e4e65e4e8c26505ef2
shaikhusama155 <shaikhusama155@gmail.com>
2023-07-19 12:00:01 +0500

    practice

--- firebase.js
+++ firebase.js
@@ -1,7 +1,28 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+// import { getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js"
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCMTPAhUuVd66aqZyqHLAXpeZu2-RoF3y0",
+//   authDomain: "first-project-2233.firebaseapp.com",
+//   projectId: "first-project-2233",
+//   storageBucket: "first-project-2233.appspot.com",
+//   messagingSenderId: "835896955200",
+//   appId: "1:835896955200:web:03f9377b73f0edd6d9aaa5",
+//   measurementId: "G-198NB1HHQQ"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
+// export const auth = getAuth(app);
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js"
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -76,17 +97,18 @@ forgetpass.addEventListener(("click", () => {
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    document.getElementById("user").innerHTML = "Hello, " + user.email;
+    document.getElementById("user").innerText = "Hello, " + user.email;
   } else {
     location.replace("index.html");
   }
 });
 
-logoutButton.addEventListener("click", () => {
-  firebase.auth().signOut().then(() => {
-    location.replace("index.html");
-  }).catch((error) => {
-    console.error("Logout error:", error);
-  });
+logoutbutton.addEventListener("click", () => {
+signOut(auth).then(() => {
+  alert('logout')
+location.replace('index.html')
+}).catch((error) => {
+  document.getElementById("user").innerHTML = error.message
+});
 });
 
--- index.js
+++ index.js
@@ -1,41 +1,77 @@
-import { getAuth } from "./firebase.js";
-import {signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"
-let loginbtn = getElementById("login").value
-let signupbtn = getElementById("signup").value
-let forgetbtn = getElementById("signup").value
+// import { auth } from "./firebase"
+// import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"
 
 
-const auth = getAuth();
-auth.onAuthStateChanged((user) => {
-    if (user) {
-        location.replace("welcome.html");
-    }
-});
-loginbtn.addEventListener(("click", () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    signInWithEmailAndPassword(auth, email, password)
-        .catch((error) => {
-            document.getElementById("error").innerHTML = error.message;
-        })
-}))
+// let loginbutton = getElementById("login")
+// let signupbtn = getElementById("signup")
+// let forgetbtn = getElementById("forgetpas")
+// const email = document.getElementById("email")
+// const password = document.getElementById("password")
 
-signupbtn, addEventListener(("click", () => {
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .catch((error) => {
-            document.getElementById("error").innerHTML = error.message
-        })
-}))
+// signupbutton.addEventListener('click', () => {
+//     const getEmail = email.value
+//     const getPass = password.value
+//     signInWithEmailAndPassword(auth, email, password)
+//         .then((userCredential) => {
+//             alert('account created')
+//             const user = userCredential.user;
+//             // ...
+//         })
+//         .catch((error) => {
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//         });
+
+// })
+
+// loginbutton.addEventListener('click', () => {
+//     const getEmail = email.value
+//     const getPass = password.value
+//     createUserWithEmailAndPassword(auth, email, password)
+//         .then((userCredential) => {
+//             alert('succussfully Login')
+//             const user = userCredential.user;
+//             // ...
+//         })
+//         .catch((error) => {
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             document.getElementById("error").innerHTML = error.message;
+//         });
+// })
+
+// // import { getAuth } from "./firebase.js";
+// // import {signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"
 
 
-forgetbtn.addEventListener(("click", () => {
-    firebase.auth().sendPasswordResetEmail(email)
-        .then(() => {
-            alert("Reset link sent to your email id")
-        })
-        .catch((error) => {
-            document.getElementById("error").innerHTML = error.message
-        });
-}))
+// // auth.onAuthStateChanged((user) => {
+// //     if (user) {
+// //         location.replace("welcome.html");
+// //     }
+// // });
+// // loginbtn.addEventListener(("click", () => {
+// //     signInWithEmailAndPassword(auth, email, password)
+// //         .catch((error) => {
+// //             document.getElementById("error").innerHTML = error.message;
+// //         })
+// // }))
+
+// // signupbtn, addEventListener(("click", () => {
+// //     const email = document.getElementById("email").value
+// //     const password = document.getElementById("password").value
+// //     firebase.auth().createUserWithEmailAndPassword(email, password)
+// //         .catch((error) => {
+// //             document.getElementById("error").innerHTML = error.message
+// //         })
+// // }))
+
+
+// // forgetbtn.addEventListener(("click", () => {
+// //     firebase.auth().sendPasswordResetEmail(email)
+// //         .then(() => {
+// //             alert("Reset link sent to your email id")
+// //         })
+// //         .catch((error) => {
+// //             document.getElementById("error").innerHTML = error.message
+// //         });
+// // }))
